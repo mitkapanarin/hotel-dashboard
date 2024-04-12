a0becebe37f63dfc40df4d7120a74348ac5b9150
mitkapanarin <mitka_j@yahoo.com>
2024-04-12 15:23:55 +0200

    create form and Add A New Hotel inr Firestore Database

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -1,8 +1,15 @@
-import { collection, getFirestore } from "firebase/firestore";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 import { app } from "./firebase";
+import { AddHotelType } from "../types/hotel";
 
 export const firestore = getFirestore(app);
 
 // HOTELS COLLECTION
 export const hotelsCollection = collection(firestore, "hotels");
+
+// ADD A NEW DOCUMENT TO YOUR COLLECTION
+export const addHotel = async (hotelData: AddHotelType) => {
+  const newHotel = await addDoc(hotelsCollection, { ...hotelData });
+  console.log(`The new hotel was created at ${newHotel.path}`);
+};
